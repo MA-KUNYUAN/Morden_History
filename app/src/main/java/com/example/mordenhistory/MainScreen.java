73309ded72441758fe482001faed01a7d7740087
Squadies <791347565@qq.com>
2020-04-05 20:02:39 +0800

    The main screen that contain the topic of the morden history

--- app/src/main/java/com/example/mordenhistory/MainScreen.java
+++ app/src/main/java/com/example/mordenhistory/MainScreen.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_screen);
         //Hi I have made the change.
         //123456
     }
