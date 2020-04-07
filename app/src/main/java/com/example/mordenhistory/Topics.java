cdd20f342ba879e6b901d7092e55578dee602680
laraluitingh <lara.luitingh@gmail.com>
2020-04-07 13:25:51 +1000

    123

---
+++ app/src/main/java/com/example/mordenhistory/Topics.java
@@ -0,0 +1,10 @@
+package com.example.mordenhistory;
+
+import java.util.ArrayList;
+
+public class Topics {
+
+    private String name;
+    private String peroid;
+    private ArrayList<String> pdf;
+}
